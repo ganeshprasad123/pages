fa44fb15c3fd95459c22a492db6f125fa23f7ed8
ganeshprasad123 <ganesh_prasad2@dell.com>
2020-09-16 07:43:26 +0100

    scalability and availability

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -3,10 +3,16 @@ set -ex
 
 kubectl apply -f deployment/pages-namespace.yaml
 
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
+
 kubectl apply -f deployment/mysql-secret.yaml
 kubectl apply -f deployment/mysql-service.yaml
+kubectl delete -f deployment/mysql-deployment.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
+sleep 30
 kubectl apply -f deployment/flyway-configmap.yaml
+kubectl delete -f deployment/flyway-job.yaml
 kubectl apply -f deployment/flyway-job.yaml
 
 kubectl apply -f deployment/pages-config.yaml
@@ -14,4 +20,5 @@ kubectl delete -f deployment/pages-service.yaml
 kubectl delete -f deployment/pages-deployment.yaml
 kubectl apply -f deployment/pages-service.yaml
 kubectl apply -f deployment/pages-deployment.yaml
+
 
